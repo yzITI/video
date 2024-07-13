a261cfb7fbaebc6b022b6877fd26800a96487640
Phantomlsh <30175757+phantomlsh@users.noreply.github.com>
2024-07-13 11:44:55 -0500

    Remove openai

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-import { transcribe, analyze } from './openai.js'
 import { extractAudio, replaceAudio, denoiseAudio, normalizeAudio } from './media.js'
 
 const prompt = ``
@@ -6,20 +5,12 @@ const prompt = ``
 const videoFileName = '../Raw/video.mp4'
 const audioFileName = '../Raw/audio.mp3'
 const outputFileName = '../Raw/output.mp4'
-const subtitleFileName = '../Raw/subtitle.srt'
-const analysisFileName = '../Raw/analysis.txt'
 
 async function main () {
   await extractAudio(videoFileName, audioFileName)
   await denoiseAudio(audioFileName, './temp.mp3')
   await normalizeAudio('./temp.mp3', audioFileName)
-  let promise = null
-  try {
-    promise = transcribe(audioFileName, subtitleFileName, prompt)
-  } catch (e) { console.log(e) }
   await replaceAudio(videoFileName, audioFileName, outputFileName)
-  await promise
-  await analyze(subtitleFileName, analysisFileName, prompt)
   console.log('# Complete')
 }
 
